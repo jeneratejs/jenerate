dbf2ae3e6f5ac60c0c01320a87780df1ffc9abea
Jake Marsh <jake@wealthfront.com>
2016-02-12 15:12:49 -0800

    begin adding test files

---
+++ test/utils/create_template_regex.spec.js
@@ -0,0 +1,13 @@
+import chai from 'chai';
+import createTemplateRegex from '../../lib/utils/create_template_regex';
+
+chai.should();
+
+describe('createTemplateRegex', () => {
+  it('should return a regex for identifying a property name in the template string', function() {
+    const regex = createTemplateRegex('test');
+    const testString = 'abcdefg<% test %>ijklmnop';
+
+    testString.replace(regex, 'h').should.equal('abcdefghijklmnop');
+  });
+});
